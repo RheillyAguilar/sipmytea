39a2db1ce25c4b4a0bfa408d6033b4a0e0332773
Rheilly Aguilar <rheilly17@gmail.com>
2025-05-03 20:32:41 +0800

    save account with ui

--- lib/page/account_page.dart
+++ lib/page/account_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sipmytea/main.dart';
 
@@ -17,16 +19,17 @@ class _LoginPageState extends State<LoginPage>
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isPasswordVisible = false;
-  bool _rememberMe = false;
+  bool _saveAccount = false;
 
   late AnimationController _controller;
   late Animation<Offset> _slideAnimation;
   late Animation<double> _fadeAnimation;
 
+  List<Map<String, String>> _savedAccounts = [];
+
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 800),
@@ -44,52 +47,54 @@ class _LoginPageState extends State<LoginPage>
 
     _controller.forward();
 
-    _loadSavedCredentials();
+    _loadSavedAccounts();
   }
 
-  void _loadSavedCredentials() async {
+  Future<void> _loadSavedAccounts() async {
     final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getStringList('savedAccounts') ?? [];
+
     setState(() {
-      _rememberMe = prefs.getBool('rememberMe') ?? false;
-      if (_rememberMe) {
-        _emailController.text = prefs.getString('savedEmail') ?? '';
-        _passwordController.text = prefs.getString('savedPassword') ?? '';
-      }
+      _savedAccounts = saved.map((entry) => Map<String, String>.from(jsonDecode(entry))).toList();
     });
   }
 
-  void _login() async {
-    String email = _emailController.text.trim();
-    String password = _passwordController.text;
+  Future<void> _login({String? email, String? password}) async {
+    final emailText = email ?? _emailController.text.trim();
+    final passwordText = password ?? _passwordController.text;
 
     try {
       final querySnapshot = await FirebaseFirestore.instance
           .collection('account')
-          .where('email', isEqualTo: email)
-          .where('password', isEqualTo: password)
+          .where('email', isEqualTo: emailText)
+          .where('password', isEqualTo: passwordText)
           .get();
 
       if (querySnapshot.docs.isNotEmpty) {
         final prefs = await SharedPreferences.getInstance();
-        if (_rememberMe) {
-          await prefs.setBool('rememberMe', true);
-          await prefs.setString('savedEmail', email);
-          await prefs.setString('savedPassword', password);
-        } else {
-          await prefs.clear();
-        }
-
         final userData = querySnapshot.docs.first.data();
         final isAdmin = userData['admin'] ?? false;
         final username = userData['username'] ?? '';
 
+        if (_saveAccount) {
+          List<String> saved = prefs.getStringList('savedAccounts') ?? [];
+
+          // Remove duplicates
+          saved.removeWhere((e) => jsonDecode(e)['email'] == emailText);
+
+          Map<String, String> newAccount = {
+            'email': emailText,
+            'password': passwordText,
+            'username': username,
+          };
+          saved.add(jsonEncode(newAccount));
+          await prefs.setStringList('savedAccounts', saved);
+        }
+
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => MainPage(
-              isAdmin: isAdmin,
-              username: username,
-            ),
+            builder: (context) => MainPage(isAdmin: isAdmin, username: username),
           ),
         );
       } else {
@@ -104,12 +109,75 @@ class _LoginPageState extends State<LoginPage>
     }
   }
 
+  Future<void> _logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('rememberMe');
+    await prefs.remove('savedEmail');
+    await prefs.remove('savedPassword');
+    setState(() {
+      _emailController.clear();
+      _passwordController.clear();
+      _saveAccount = false;
+    });
+    _loadSavedAccounts(); // refresh UI
+  }
+
   @override
   void dispose() {
     _controller.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
+  }
+
+  Widget _buildSavedAccountsUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Wrap(
+          spacing: 20,
+          children: [
+            ..._savedAccounts.map((account) {
+              return GestureDetector(
+                onTap: () => _login(
+                  email: account['email'],
+                  password: account['password'],
+                ),
+                child: Column(
+                  children: [
+                    const CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Color(0xFF4B8673),
+                      child: Icon(Iconsax.user, size: 40, color: Colors.white),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(account['username'] ?? ''),
+                  ],
+                ),
+              );
+            }),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _savedAccounts.clear(); // hide saved UI, show login form
+                });
+              },
+              child: Column(
+                children: const [
+                  CircleAvatar(
+                    radius: 35,
+                    backgroundColor: Color(0xFF4B8673),
+                    child: Icon(Iconsax.add, size: 40, color: Colors.white),
+                  ),
+                  SizedBox(height: 8),
+                  Text("Add account"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
   @override
@@ -124,89 +192,91 @@ class _LoginPageState extends State<LoginPage>
               position: _slideAnimation,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/sipmytea_logo.png', height: 120),
-                    const SizedBox(height: 32),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: const Icon(Icons.person),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: !_isPasswordVisible,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        prefixIcon: const Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
+                child: _savedAccounts.isNotEmpty
+                    ? _buildSavedAccountsUI()
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/sipmytea_logo.png', height: 120),
+                          const SizedBox(height: 32),
+                          TextField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              prefixIcon: const Icon(Icons.person),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
                           ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordVisible = !_isPasswordVisible;
-                            });
-                          },
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Checkbox(
-                          value: _rememberMe,
-                          onChanged: (value) {
-                            setState(() {
-                              _rememberMe = value!;
-                            });
-                          },
-                          activeColor: const Color(0xFF4B8673),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4),
+                          const SizedBox(height: 16),
+                          TextField(
+                            controller: _passwordController,
+                            obscureText: !_isPasswordVisible,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              prefixIcon: const Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
                           ),
-                        ),
-                        const Text('Remember Me', style: TextStyle(fontSize: 15)),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: _login,
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: _saveAccount,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _saveAccount = value!;
+                                  });
+                                },
+                                activeColor: const Color(0xFF4B8673),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                              ),
+                              const Text('Save account', style: TextStyle(fontSize: 15)),
+                            ],
                           ),
-                          backgroundColor: const Color(0xFF4B8673),
-                        ),
-                        child: const Text(
-                          'Log in',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                        ),
+                          const SizedBox(height: 16),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: _login,
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(vertical: 14),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                backgroundColor: const Color(0xFF4B8673),
+                              ),
+                              child: const Text(
+                                'Log in',
+                                style: TextStyle(fontSize: 16, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
               ),
             ),
           ),
